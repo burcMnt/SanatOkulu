b64e87246c69484d9823dc6bfb97b1f1a7b4c6a1
burcMnt <burcumantar0@gmail.com>
2021-06-29 12:32:16 +0300

    EF Dizayn

---
+++ SanatOkulu/Models/Eser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanatOkulu.Models
+{
+    [Table("Eserler")]
+    public class Eser
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(50)]
+        public string Ad { get; set; }
+        public int? Yil { get; set; }
+
+        //Sanatci + Id : Sanatci entitysinin ıDsine referans veren bir Fk
+        public int SanatciId { get; set; }
+
+        public virtual Sanatci Sanatci { get; set; } //Bu eserin 1 sanatçisi vardır(1-çok ilişki)
+    }
+}
---
+++ SanatOkulu/Models/Sanatci.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanatOkulu.Models
+{
+    [Table("Sanatcilar")]
+    public class Sanatci
+    {
+        public int Id { get; set; }
+        [Required,MaxLength(50)]
+        public string Ad { get; set; }
+
+        //virtual konduğunda bu navigation property de Lazy Loading aktif olur bu ihtiyaç halinde yükleme yapar
+        //yani ben diger tarafta formlarda çağırmadıgım sürece foreach ile yükleme yapmaz
+        //sadece çağırılırsa sql den joinleyip seçilip getiriliyor
+        public virtual ICollection<Eser> Eserler { get; set; } //bu sanatçının birden çok eseri olabilir
+    }
+}
